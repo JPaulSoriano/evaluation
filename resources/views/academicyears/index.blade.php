0d05c6be63796b74feca678c785f3964835a3ffe
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2022-10-11 16:43:32 +0800

    update

--- resources/views/academicyears/index.blade.php
+++ resources/views/academicyears/index.blade.php
@@ -13,13 +13,28 @@
         <tr>
             <th>No</th>
             <th>Name</th>
+            <th>Status</th>
+            <th width="100px">Action</th>
         </tr>
-        @foreach ($academicyears as $academicyear)
         <tr>
-            <td>{{ ++$i }}</td>
-            <td>{{ $academicyear->name }}</td>
+            <td>3</td>
+            <td>2022-2023</td>
+            <td><label class="badge badge-danger">Not Yet Started</label></td>
+            <td><a href=""class="btn btn-sm btn-block btn-success my-1">Start</a></td>
         </tr>
-        @endforeach
+        <tr>
+            <td>2</td>
+            <td>2021-2022</td>
+            <td><label class="badge badge-success">Active</label></td>
+            <td><a href=""class="btn btn-sm btn-block btn-danger my-1">End</a></td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2019-2021</td>
+            <td><label class="badge badge-secondary">Ended</label></td>
+            <td><a href=""class="btn btn-sm btn-block btn-secondary my-1 disabled">Start</a></td>
+        </tr>
+
     </table>
 
     {!! $academicyears->links() !!}
