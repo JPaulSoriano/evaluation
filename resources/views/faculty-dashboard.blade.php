3186615b034290aa1d0f54b1477f55ccb26d99a4
JPaulSoriano <johnpaul.soriano.6197@gmail.com>
2023-07-09 07:20:51 +0800

    update

--- resources/views/faculty-dashboard.blade.php
+++ resources/views/faculty-dashboard.blade.php
@@ -7,7 +7,7 @@
         </div>
     </div>
     <div class="alert alert-success" role="alert">
-        Current Faculty Evaluation Academic Year: <span class="font-weight-bold">{{ $academicyears->name }}</span>
+        Current Faculty Evaluation Academic Year: <span class="font-weight-bold">{{ $academicyears }} {{ $quarter }} Quarter</span>
       </div>
     <div class="jumbotron">
         <h1 class="mb-5">Welcome!  <span class="font-weight-bold">{{ Auth::user()->full_name }}</span></h1>
